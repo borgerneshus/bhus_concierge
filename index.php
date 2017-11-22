f116388a2a71a70183c64a9aabc7bc5d3c111b50
Tobias F. Petersen <tobsf@hotmail.com>
2017-11-22 14:17:45 +0100

    fix weekend check

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ try {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        
+        <link rel="stylesheet" href="/css/global.css">
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>      
         <!-- Latest compiled and minified JavaScript -->
@@ -46,7 +46,7 @@ try {
 		?>
         <meta charset="utf-8" />
     </head>
-    <body class="col-md-12 ">
+    <body class="col-md-12 removemargin removepadding">
     	<?php include_once("templates/" . $template . "/gui.php") ?>
     </body>
 </html>
