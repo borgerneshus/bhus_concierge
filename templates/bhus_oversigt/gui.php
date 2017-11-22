5c278bcf3340e68d25dcee4d106f3ffa9a318c73
Tobias F. Petersen <tobsf@hotmail.com>
2017-11-22 14:21:43 +0100

    fix weekend check

--- templates/bhus_oversigt/gui.php
+++ templates/bhus_oversigt/gui.php
@@ -1,8 +1,8 @@
-<div id="wrapper" class="col-md-12 " style="margin:0px;padding:5px;">
-            <div id="top-bar" class="col-md-12 navbar-fixed-top">
-                  <div id="logo" class="col-md-12">
-                    <div class="col-md-3"><img style="float:left;"  src="bhus_booking_events(1).png"></div>
-                    <div class="col-md-5 align-center"><h1>Det sker i dag</h1></div>  <div class="col-md-4" style="float:right;text-align: right;"><h1><?php echo !isTodayWeekend() ? "Åbent 8-21" : "Åbent 10-16"; ?></h1></div>
+<div id="wrapper" class="col-md-12 removemargin removepadding" style="margin:0px;padding:5px;">
+            <div id="top-bar" class="col-md-12 navbar-fixed-top removemargin removepadding">
+                  <div id="logo" class="col-md-12 removepadding">
+                    <div class="col-md-3 removepadding"><img style="float:left;"  src="bhus_booking_events(1).png"></div>
+                    <div class="col-md-5 align-center"><h1>Det sker i dag</h1></div>  <div class="col-md-4 removepadding" style="float:right;text-align: right;"><h1><?php echo !isTodayWeekend() ? "Åbent 8-21" : "Åbent 10-16"; ?></h1></div>
                   </div>
             </div>
             <div class="col-md-12" style="margin-top:150px;">
