f1b762e2a0f3631f0e8f546dad4756da792ba1e3
Tobias F. Petersen <tobsf@hotmail.com>
2017-11-22 12:54:20 +0100

    prep for production

--- statestik.php
+++ statestik.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 
-include_once 'bhus_concierge.php';
+include_once 'includes/bhus_concierge.php';
 
 $_GET['targetmailbox'] =  isset($_GET['targetmailbox']) ? $_GET['targetmailbox'] :"lok11_borghus@odense.dk,lok12_borghus@odense.dk,lok21_borghus@odense.dk,lok22_borghus@odense.dk,lok31_borghus@odense.dk,lok32_borghus@odense.dk,lok33_borghus@odense.dk,lok34_borghus@odense.dk,lok35_borghus@odense.dk,lok36_borghus@odense.dk";
 $_GET['start'] =  isset($_GET['start']) ? $_GET['start'] :'January 1 00:00:00'; 
@@ -18,6 +18,7 @@ $result = $concierge->GetByServiceInput(true);
  * Make it serializeable
  */
 $encode_test = array();
+$test = json_encode($obj);
 /*
  * We do this because php json_encode can not decode the more complex
  * objects from the php-ews library.
