649d28c0b407a38ef36fa6008229cc9ca3de199b
Tobias F. Petersen <tobsf@hotmail.com>
2018-09-21 09:29:36 +0200

    tmp file: esport_footer.php needed for other project

---
+++ esport_footer.php
@@ -0,0 +1,35 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <title></title>
+        <style>
+            body{
+                background-color:black;
+                color: white;
+                font-size:25px;
+            }
+            .dot {
+                height: 20px;
+                width: 20px;
+                background-color: red;
+                border-radius: 50%;
+                display: inline-block;
+                margin-right: 5px;
+              }
+        </style>
+    </head>
+    <body>
+            <div>
+            <div class="dot"></div>
+            <div>Kom og se E-sport live i stuen!</div>
+            <div style="float:right">Logo</div>
+            </div>
+    </body>
+</html>
