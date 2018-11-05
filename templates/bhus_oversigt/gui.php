4b16da8e6757426878f6e7e83f1b3106069df8b8
memleakz <dev@dev-VirtualBox>
2018-11-05 08:16:20 +0100

    fix size

--- templates/bhus_oversigt/gui.php
+++ templates/bhus_oversigt/gui.php
@@ -10,8 +10,8 @@ $calenda//r_events = $b->EnrichWithCustomEvents($calendar_events);
     <div id="top-bar" class="col-sm-12 col-lg-12 col-md-12 removemargin">
         <div id="logo" class="col-sm-12 col-lg-12 col-md-12 removepadding">
             <div class="col-sm-2 col-lg-2 col-md-2 removepadding"><img style="float:left;height: 100px;margin-top: 18px;"  src="bhus_booking_events(1).png"></div>
-            <div class="col-sm-4 col-lg-7 col-md-4 align-center "><h1 class="middle-text" style="padding-top:15px !important;" >Mødeoversigt</h1></div>  
-            <div class="col-sm-3 col-lg-3 col-md-12 align-center"><h1 class="right-text" style="pading-top:13px !important;font-size:40px !important;padding-left:150px;" ><?php echo !$b->isTodayWeekend() ? "Åbent 8-21" : "Åbent 10-16"; ?></h1></div>
+            <div class="col-sm-4 col-lg-7 col-md-7 align-center "><h1 class="middle-text" style="padding-top:15px !important;" >Mødeoversigt</h1></div>  
+            <div class="col-sm-3 col-lg-3 col-md-3 align-center"><h1 class="right-text" style="pading-top:13px !important;font-size:40px !important;padding-left:150px;" ><?php echo !$b->isTodayWeekend() ? "Åbent 8-21" : "Åbent 10-16"; ?></h1></div>
         </div>
     </div>
     <br/>
